63696e07adbf4ddd32697d0aecb293099504aa49
jv <jeff@jeffvier.com>
2014-11-05 13:15:40 -0800

    no idea how this got so broken.  I blame git merge hell.

--- manifests/value.pp
+++ manifests/value.pp
@@ -17,32 +17,10 @@ define sysctl::value (
   require sysctl::base
   $val1 = inline_template("<%= @value.split(/[\s\t]/).reject(&:empty?).flatten.join(\"\t\") %>")
 
-  $array = split($value,'[\s\t]')
-  $val1 = inline_template("<%= @array.delete_if(&:empty?).flatten.join(\"\t\") %>")
-
-  $real_key = $key ? {
-    'name'  => $name,
-    default => $key,
-  }
-
-  sysctl { $real_key :
-    val    => $val1,
-    before => Exec["exec_sysctl_${real_key}"],
-  }
-
-  $command = $::kernel ? {
-    openbsd => "/sbin/sysctl ${real_key}=\"${val1}\"",
-    default => "/sbin/sysctl -w ${real_key}=\"${val1}\"",
-  }
-
-  $unless = $::kernel ? {
-    openbsd => "/sbin/sysctl ${real_key} | grep -q '=${val1}\$'",
-    default => "/sbin/sysctl ${real_key} | grep -q ' = ${val1}'",
-  }
-
-  exec { "exec_sysctl_${real_key}" :
-      command => $command,
-      unless  => $unless,
-      require => Sysctl[$real_key],
+  sysctl { $key :
+    val     => $val1,
+    target  => $target,
+  } -> sysctl_runtime { $key:
+    val => $val1,
   }
 }
